0c4f23d2738aa11d447c49e2883d293dbddceb09
Thaynara Barcelos <vitoriathaynara63@gmail.com>
2024-11-06 17:59:25 -0300

    Trabalhando com rotas

--- routes/funcionario.js
+++ routes/funcionario.js
@@ -7,13 +7,19 @@ const departamento = require('../models/departamento')
 
 // CRIANDO AS ROTAS
 // 1º ROTA - INSERIR DADOS NA TABELA
-router.get('/store', async(req, res)=> {
+router.post('/store', async(req, res)=> {
     const resultado = await funcionario.create({
-        nome:'Joana Darc',
-        salario:3000,
-        cargo:'Analista de Sistemas',
-        departamentoId: 1 // Esse campo é a chave estrangeira
+        nome:req.body.nome,
+        salario: req.body.salario,
+        cargo: req.body.cargo,
+        departamentoId: req.body.departamento // Esse campo é a chave estrangeira
     })
+    if(resultado){
+        resultado.redirect('/')
+    }
+    else{
+        res.json({erro:"Os dados não foram cadastrados no banco"})
+    }
 })
 
 // 2ª ROTA - EXIBIR A PÁGINA INICIAL DO FUNCIONÁRIO
@@ -32,5 +38,21 @@ router.get('/show', async(req, res)=> {
         console.log('Não foi possível exibir os dados')
     }
 })
+
+//4ª ROTA - DELETAR  DADOS NA TABELA
+//:id siginifica que iremos passar um valor na rota,ou seja diferente e que será armazenado pela variável: id
+router.get('/destroy/:id',async (req,res) =>{
+    const resultado = await funcionario.destroy({
+        where:{
+            id:req.params.id //estamos recebendo o id via parâmetro que está sendo passado na rota, no caso, é o :id que estamos recebendo (FUNCIONÁRIO)
+    }
+})
+})
+
+// 5ª ROTA -EXIBIR FORMULÁRIO DE CADASTRO 
+router.get('/create', (req, res) => {
+    res.render('funcionario/addFuncionario')
+})
+
 
 module.exports = router 
