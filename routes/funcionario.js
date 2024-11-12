39c26d5a8d0b9e614c6b9e1db6a4d3fa033c1a5a
Yasmin Silva <yasminevilyn23@gmail.com>
2024-11-12 14:50:47 -0300

    Trabalho Final

--- routes/funcionario.js
+++ routes/funcionario.js
@@ -15,7 +15,7 @@ router.post('/store', async(req, res)=> {
         departamentoId: req.body.departamento // Esse campo é a chave estrangeira
     })
     if(resultado){
-        resultado.redirect('/')
+        res.redirect('/')
     }
     else{
         res.json({erro:"Os dados não foram cadastrados no banco"})
@@ -23,16 +23,17 @@ router.post('/store', async(req, res)=> {
 })
 
 // 2ª ROTA - EXIBIR A PÁGINA INICIAL DO FUNCIONÁRIO
-router.get('/', (req, res)=>{
+router.get('/show', (req, res)=>{
     res.send('<h1> Página Inicial do Funcionário </h1>')
 })
 
 // 3ª ROTA - CONSULTAR DADOS NA TABELA
-router.get('/show', async(req, res)=> {
-    const resultado = await funcionario.findAll()
+router.get('/', async(req, res)=> {
+    const resultado = await funcionario.findAll({include:departamento}) // o include:departamento é como o sequelize faz para poder realizar consultas com join
 
     if (resultado){
         console.log(resultado)
+        res.render("funcionario/index", {dados:resultado})
     }
     else{
         console.log('Não foi possível exibir os dados')
@@ -46,12 +47,21 @@ router.get('/destroy/:id',async (req,res) =>{
         where:{
             id:req.params.id //estamos recebendo o id via parâmetro que está sendo passado na rota, no caso, é o :id que estamos recebendo (FUNCIONÁRIO)
     }
-})
+    })
+    res.redirect("/")
 })
 
 // 5ª ROTA -EXIBIR FORMULÁRIO DE CADASTRO 
-router.get('/create', (req, res) => {
-    res.render('funcionario/addFuncionario')
+router.get('/create',async (req, res) => {
+    let resultado = await departamento.findAll()
+
+    if(resultado){
+        res.render('funcionario/addFuncionario' ,{dados:resultado})
+    }
+    else{
+        console.log("Não foi possível carregar os dados")
+        res.redirect('/') //redirecionando para a página inicial
+    }
 })
 
 
